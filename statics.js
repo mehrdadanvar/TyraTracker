a00aff6f1f04bc10928d870baba194d6e39436b4
mehrdad.anvar <mehrdad.anvar@gmail.com>
2023-06-11 11:30:11 -0700

    this file includes static assests including links, urls, headers on which the main classes rely

---
+++ statics.js
@@ -0,0 +1,26 @@
+const statics = {
+  price_url: "https://apim.canadiantire.ca/v1/product/api/v1/product/sku/PriceAvailability/",
+  ct_headers: {
+    authority: "apim.canadiantire.ca",
+    accept: "application/json, text/plain, */*",
+    "accept-language": "en-US,en;q=0.9,fa-IR;q=0.8,fa;q=0.7,es;q=0.6",
+    bannerid: "CTR",
+    basesiteid: "CTR",
+    "content-type": "application/json",
+    "ocp-apim-subscription-key": "c01ef3612328420c9f5cd9277e815a0e",
+    origin: "https://www.canadiantire.ca",
+    referer: "https://www.canadiantire.ca/",
+    "sec-ch-ua": '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "Linux",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-site",
+    "service-client": "ctr/web",
+    "service-version": "ctc-dev2",
+    "user-agent":
+      "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+    "x-web-host": "www.canadiantire.ca",
+  },
+};
+module.exports = statics;
