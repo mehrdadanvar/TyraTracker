ff0a8f467b20242a7c9d208994938d2784b36dcd
mehrdad.anvar <mehrdad.anvar@gmail.com>
2023-06-11 11:37:11 -0700

    added timer helper function and two static properties

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ class CT {
   constructor(name) {
     this.name = name;
   }
+  ///////////////// Statics ///////////////////////////////////////
+  static request_headers = loaded_statics.ct_headers;
+  static price_url = loaded_statics.price_url;
+  static timer = (ms) => new Promise((res) => setTimeout(res, ms));
+  /////////////////////////////////////////////////////////////////
 }
 // console.log(CT);
 module.exports = CT;
