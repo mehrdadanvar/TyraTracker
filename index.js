c1edb9324491db8109c662946f362f57d6efada1
mehrdad.anvar <mehrdad.anvar@gmail.com>
2023-06-11 11:38:52 -0700

    added load_product_codes function which loads 45000 product codes

--- index.js
+++ index.js
@@ -13,6 +13,23 @@ class CT {
   static price_url = loaded_statics.price_url;
   static timer = (ms) => new Promise((res) => setTimeout(res, ms));
   /////////////////////////////////////////////////////////////////
+  static async load_product_codes() {
+    try {
+      const database = client.db("codes");
+      const codes = database.collection("codes");
+      const skus = codes.find();
+      let cleaned_codes = [];
+      for await (const sku of skus) {
+        cleaned_codes.push(sku.codes);
+      }
+      return cleaned_codes[0];
+    } catch (error) {
+      error ? console.log(error) : "";
+    } finally {
+      await client.close();
+    }
+  }
+  /////////////////////////////////////////////////////////////////
 }
 // console.log(CT);
 module.exports = CT;
